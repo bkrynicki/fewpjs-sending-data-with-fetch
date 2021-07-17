49956cf2749d1fca8212f8b1020b5888b1e1e4b4
Brandon <bjkrynicki@gmail.com>
2021-07-17 16:45:59 -0500

    commit tree 86d9319972ae22053b6e380ff22d7660fad22385

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let body = document.querySelector("body");
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com",
+  }),
+};
+function submitData() {
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then(function (object) {
+      body.append(object.id);
+    })
+    .catch(function (error) {
+      alert(`Unauthorized Access`);
+      body.append(error.message);
+    });
+}
